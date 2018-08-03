2fe248a2b498c00cd15465e6ceea1cc6788c73d3
Michael Boroski <mjboroski@gmail.com>
2018-08-03 10:05:05 +0000

    automatically backed up by learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -24,7 +24,7 @@ class CreateTodo extends Component {
   render() {
     return(
       <div>
-        <form>
+        <form onSubmit={ event => this.handleSubmit(event) }>
           <p>
             <label>add todo</label>
             <input type="text" onChange={(event) => this.handleChange(event)} value={this.state.text}/>
